e6dce5b9c53f7d36b3d8b58aaf8ff2da2ef52ac2
Satishsatti3789 <satishdevops3789@gmail.com>
2024-09-07 20:47:50 +0530

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -2,17 +2,22 @@ source common.sh
 component=redis
 
 echo -e "${color} Install ${component} repos ${nocolor}"
-sudo yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${logfile}
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${logfile}
+stat_check $?
 
 echo -e " ${color}  Enable $(component) 6 Version  ${nocolor} "
-sudo yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log &>>${logfile}
+yum module enable ${component}:remi-6.2 -y &>>/tmp/roboshop.log &>>${logfile}
+stat_check $?
 
 echo -e "${color} Install ${component}  ${nocolor}"
-sudo yum install redis -y &>>${logfile}
+sudo yum install ${component} -y &>>${logfile}
+stat_check $?
 
 echo -e "${color} Modify ${component} configuration file ${nocolor}"
-sed -i 's/127.0.0.0/0.0.0.0' /etc/redis.conf  /etc/redis/redis.conf &>>${logfile}
+sed -i 's/127.0.0.0/0.0.0.0' /etc/${component}.conf  /etc/${component}/${component}.conf &>>${logfile}
+stat_check $?
 
 echo -e "${color} Start  ${component} service ${nocolor}"
-systemctl enable redis &>>${logfile}
-systemctl start redis &>>${logfile}
+systemctl enable ${component} &>>${logfile}
+systemctl start ${component} &>>${logfile}
+stat_check $?
