5c3d746dee59ba71db2cafe4dca6d51c703acef1
Satishsatti3789 <satishdevops3789@gmail.com>
2024-09-07 20:39:20 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -2,13 +2,16 @@ source common.sh
 component=mongod
 
 echo -e "${color} copy ${component} repo file ${nocolor}"
-sudo cp -r /home/centos/roboshop-scripts/mongodb.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
+cp /home/centos/roboshop-scripts/mongodb.repo /etc/yum.repos.d/mongo.repo  &>>${log_file}
+stat_check $?
 
 echo -e "${color} install ${component} repo file ${nocolor}"
-sudo yum install mongodb-org -y &>>${log_file}
+yum install mongodb-org -y &>>${log_file}
+stat_check $?
 
 echo -e "${color}  update ${component} conf file ${nocolor}"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/${component}.conf  &>>${log_file}
+stat_check $?
 
 echo -e "${color} starting ${component} service ${nocolor}"
 systemctl enable ${component}  &>>${log_file}
