2105a4ccb64a3686638806f2c138300a8e739bc2
Satishsatti3789 <satishdevops3789@gmail.com>
2024-09-07 20:46:01 +0530

    Update rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,20 +3,22 @@ component=rabbitmq
 
 echo -e " ${color}  Configure Erlang repos  ${nocolor} "
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log_file}
+stat_check $?
 
 echo -e " ${color}  Configure RabbitMQ Repos  ${nocolor} "
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log_file}
 
 
 echo -e " ${color}  Install RabbitMQ Server  ${nocolor} "
-sudo yum install rabbitmq-server -y &>>${log_file}
-
+yum install rabbitmq-server -y &>>${log_file}
+stat_check $?
 
 echo -e " ${color}  Start RabbitMQ Service  ${nocolor} "
-sudo systemctl enable rabbitmq-server &>>${log_file}
-sudo systemctl restart rabbitmq-server &>>${log_file}
-
+systemctl enable rabbitmq-server &>>${log_file}
+systemctl restart rabbitmq-server &>>${log_file}
+stat_check $?
 
 echo -e " ${color}  Add RabbitMQ Application User ${nocolor} "
 rabbitmqctl add_user roboshop $1 &>>${log_file}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log_file}
+stat_check $?
